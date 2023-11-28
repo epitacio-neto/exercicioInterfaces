6db0f406ac337f4f392ba5e5bf0a8d739c856961
neto0w <epitacio.neto.92@gmail.com>
2023-11-28 09:22:21 -0300

    exercicio concluido

--- src/model/services/DomainException.java
+++ src/model/services/DomainException.java
@@ -1,6 +1,8 @@
 package model.services;
 
 public class DomainException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
 	public DomainException(String msg) {
 		super(msg);
 	}
